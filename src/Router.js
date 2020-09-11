97197dcb6cc210530e01f24b7e4b84c730e0eb91
HaoYuan-Hu <1124730278@qq.com>
2020-09-11 21:26:13 +0800

    fun: add Route & ajax & Views
    实现了部分Route，ajax，待完善；
    添加了一些Views，待实现；

---
+++ src/Router.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Router, Route, Switch, Redirect} from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
+import HomeView from "./views/homeView/HomeView";
+import LoginView from './views/loginView/LoginView';
+import RegisterView from './views/registerView/RegisterView';
+import {history} from "./utils/history";
+
+
+class BasicRoute extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        //history监听路由
+        history.listen((location, action) => {
+            // clear alert on location change
+            console.log(location,action);
+        });
+    }
+
+    render(){
+        return(
+            <Router history={history}>
+                <Switch>
+                    <PrivateRoute exact path="/" component={HomeView} />
+                    <Route exact path="/login" component={LoginView} />
+                    <Route exact path="/register" component={RegisterView} />
+                    <Redirect from="/*" to="/" />
+                </Switch>
+
+            </Router>
+        )
+    }
+
+
+}
+
+export default BasicRoute;
