44530a4b85e2fe1a774e0a4cd0d37f4b5c985322
SeanChao <seanchao0804@gmail.com>
2020-09-12 13:54:11 +0800

    feat: user activity page
    feat: components in this commit card(css class), follow button, user avatar(large)
    material-ui/core and icon are added to package files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import Activity from './views/Activity/Activity';
+import { blueTheme } from './utils/theme';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={blueTheme}>
+        {/* For Demo Purpose */}
+        <Activity />
+      </ThemeProvider>
     </div>
   );
 }
