7c8f0ad156bd75f35d82e5e701f8a3cd87e9b750
SeanChao <seanchao0804@gmail.com>
2020-09-26 17:04:14 +0800

    feat: rich text editor
    prop-types validation is temporarily disabled in some files, some bugs remain, e.g md-rich transformation is non-idempotent

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, Redirect, Route, Router, Switch } from 'react-router-dom';
 
-import ProfileHeader from './component/Profile/ProfileHeader';
+import { Editor } from './component/Editor';
 import PrivateRoute from './PrivateRoute';
 import { history } from './utils/history';
 import Activity from './views/Activity/Activity';
@@ -10,13 +10,6 @@ import LoginView from './views/loginView/LoginView';
 import { QuestionView } from './views/Question';
 import RegisterView from './views/registerView/RegisterView';
 
-const profileDemoData = {
-  username: 'QAQ 小编',
-  gender: 1,
-  img: 'https://avatars1.githubusercontent.com/u/71007591',
-  intro: '哦！我向上帝发誓，我什么也不知道。我打赌，我会把QAQ问到倒闭！看在玛丽亚的份上，请回答我的问题吧！',
-};
-
 const BasicRoute = () => (
   <>
     <Router history={history}>
@@ -24,6 +17,9 @@ const BasicRoute = () => (
         This part is for development preview purpose when the homepage is not fully implemented. Remove this section after the HomeView is done.
         <Link to="/people">用户动态(Avatar, Follow Button, Profile Header)</Link>
         <Link to="/question">问题页</Link>
+        <div>
+          <Link to="/dev/editor">Editor</Link>
+        </div>
       </div>
       <Switch>
         <PrivateRoute exact path="/" component={HomeView} />
@@ -33,8 +29,8 @@ const BasicRoute = () => (
         <Route path="/people">
           <Activity />
         </Route>
-        <Route path="/dev/profileHeader">
-          <ProfileHeader data={profileDemoData} />
+        <Route path={'/dev/editor'}>
+          <Editor />
         </Route>
         <Route exact path="/login" component={LoginView} />
         <Route exact path="/register" component={RegisterView} />
