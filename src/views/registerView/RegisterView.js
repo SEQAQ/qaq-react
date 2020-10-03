f05f2797d60fcf1b878770dd5cd4e9823d5a37eb
Xingyu <sdjm_wxy@sjtu.edu.cn>
2020-10-03 19:34:09 +0800

    feat: add the login and register pages

---
+++ src/views/registerView/RegisterView.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        QAQ.Inc
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <PersonAddIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          新用户注册
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField autoComplete="uname" name="userName" variant="outlined" required fullWidth id="userName" label="用户名" autoFocus />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField variant="outlined" required fullWidth id="nackName" label="昵称" name="nackName" autoComplete="nname" />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField variant="outlined" required fullWidth id="email" label="电子邮箱" name="email" autoComplete="email" />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField variant="outlined" required fullWidth name="password" label="密码" type="password" id="password" autoComplete="current-password" />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel control={<Checkbox value="allowExtraEmails" color="primary" />} label={'已阅读并同意'} />
+              <Link color="primary" href="https://material-ui.com/">
+                《隐私保护协议》
+              </Link>
+              <Link color="primary" href="https://material-ui.com/">
+                《QAQ用户协议》
+              </Link>
+            </Grid>
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+            注册
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                已有QAQ账号？点击这里登录
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
