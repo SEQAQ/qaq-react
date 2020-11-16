cc660fc4417e568e179ea3c4c4621abdc31088cb
SeanChao <seanchao0804@gmail.com>
2020-11-16 14:10:23 +0800

    feat: comments for answers, a basic text input component
    chore: close eslint rule react/prop-types

--- src/component/Bar/ActionBar.js
+++ src/component/Bar/ActionBar.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 
 import { CommentButton, VoteButtonGroup } from '../Button';
 
-const ActionBar = () => {
+const ActionBar = ({ commentClick }) => {
   const [vote, setVote] = useState(0);
 
   return (
     <div className="feed-item-action">
       <VoteButtonGroup vote={vote} onUp={() => setVote(1)} onDown={() => setVote(2)} onCancel={() => setVote(0)} />
       <div style={{ marginLeft: 'auto' }}>
-        <CommentButton />
+        <CommentButton onClick={commentClick} />
       </div>
     </div>
   );
