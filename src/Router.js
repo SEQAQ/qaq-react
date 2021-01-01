c5d26f93a8836b6b0e3f506b1d9c6dbf299e46b0
Xingyu <sdjm_wxy@sjtu.edu.cn>
2021-01-01 22:50:45 +0800

    feat: finish func userInfo() to get login status and user info

--- src/Router.js
+++ src/Router.js
@@ -2,8 +2,7 @@ import Grid from '@material-ui/core/Grid';
 import React from 'react';
 import { Redirect, Route, Router, Switch } from 'react-router-dom';
 
-import AppBar from '../src/component/AppBar/AppBar';
-// import SearchBar from './component/SearchBar/SearchBar';
+import SearchBar from './component/SearchBar/SearchBar';
 /* import PrivateRoute from './PrivateRoute'; */
 /* import PrivateRoute from './PrivateRoute'; */
 import { history } from './utils/history';
@@ -20,11 +19,11 @@ import SearchView from './views/SearchView/SearchView';
 const BasicRoute = () => (
   <>
     <Router history={history}>
-      {/* <SearchBar />*/}
+      <SearchBar />
       <Grid container direction="column" justify="flex-start" alignItems="center">
-        <Grid item>
-          <AppBar />
-        </Grid>
+        {/* <Grid item>*/}
+        {/*  <AppBar />*/}
+        {/* </Grid>*/}
         <Grid item>
           <Switch>
             <Route exact path="/" component={HomeView} />
--- src/lib/userStatInfo.js
+++ src/lib/userStatInfo.js
@@ -2,14 +2,20 @@ import axios from 'axios';
 import cookie from 'react-cookies';
 
 import config from '../utils/config';
-import { get } from './request';
 
 axios.defaults.baseURL = config.apiUrl;
 
 export const userInfo = () => {
   const account = cookie.load('account');
+  let userInfo = null;
   if (account === null) {
     return null;
   }
-  get(config.apiUrl + '/users/findbyaccount', account);
+  if (!window.localStorage) {
+    alert('浏览器不支持localstorage');
+    userInfo = cookie.load('userInfo');
+  } else {
+    userInfo = localStorage.getItem('userInfo');
+  }
+  return JSON.parse(userInfo);
 };
