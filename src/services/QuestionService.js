78d6a378193d6b2b4d26174a5a39bc0b7222aedc
HuHao-Yuan <1124730278@qq.com>
2021-01-08 13:57:54 +0800

    fix

--- src/services/QuestionService.js
+++ src/services/QuestionService.js
@@ -1,5 +1,18 @@
 import { get, post } from '../lib';
-import { API_QUES_CLOSE, API_QUES_DEL, API_QUES_FOLLOW, API_QUES_GET, API_QUES_GET_FOLLOWED, API_QUES_GET_USER, API_QUES_IS_FOLLOWED, API_QUES_OPEN, API_QUES_UNFOLLOW, API_HOT_GET, API_QUES_FINDALL, API_QUES_RECOM, API_QUES_UNFOLLOW } from '../utils/constants';
+import {
+  API_HOT_GET,
+  API_QUES_CLOSE,
+  API_QUES_DEL,
+  API_QUES_FINDALL,
+  API_QUES_FOLLOW,
+  API_QUES_GET,
+  API_QUES_GET_FOLLOWED,
+  API_QUES_GET_USER,
+  API_QUES_IS_FOLLOWED,
+  API_QUES_OPEN,
+  API_QUES_RECOM,
+  API_QUES_UNFOLLOW,
+} from '../utils/constants';
 import { getUser } from './userServices';
 
 export const parseQuestionData = (data) => {
@@ -18,7 +31,6 @@ export const unfollowQuestion = (qid) => post(API_QUES_UNFOLLOW + `?uid=${getUse
 
 export const getUserQuestions = (uid) => get(API_QUES_GET_USER, { uid });
 
-
 export const getAllQuestions = () => get(API_QUES_FINDALL);
 
 export const getRecomQues = (uid) => get(API_QUES_RECOM, { uid });
@@ -34,4 +46,3 @@ export const closeQuestion = (qid) => get(API_QUES_CLOSE, { qid }, true);
 export const openQuestion = (qid) => get(API_QUES_OPEN, { qid }, true);
 
 export const deleteQuestion = (qid) => get(API_QUES_DEL, { qid }, true);
-
